749292f7fd75cbace856aa7e4cc264aa1b484af7
authchir <martin.desharnais@gmail.com>
2011-11-04 16:13:10 -0400

    Complète la classe Carte.

--- mainDePocker/src/ets/log120/tp3/Carte.java
+++ mainDePocker/src/ets/log120/tp3/Carte.java
@@ -15,14 +15,55 @@ public class Carte
 	// Accesseur(s)
 	// --------------------------------------------------
 
+	public CouleurCarte getCouleur() {
+		return couleur;
+	}
+	
+	public Denomination getDenomination() {
+		return denomination;
+	}
+	
+	@Override
+	public String toString() {
+		return "Carte [couleur=" + couleur + ", denomination=" + denomination + "]";
+	}
+	
+
 	@Override
 	public int compareTo(Carte obj) {
 		return 0;
 	}
 	
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((couleur == null) ? 0 : couleur.hashCode());
+		result = prime * result
+				+ ((denomination == null) ? 0 : denomination.hashCode());
+		return result;
+	}
+
+	@Override
 	public boolean equals(Object obj) {
-		return false;
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Carte other = (Carte) obj;
+		if (couleur == null) {
+			if (other.couleur != null)
+				return false;
+		} else if (!couleur.equals(other.couleur))
+			return false;
+		if (denomination == null) {
+			if (other.denomination != null)
+				return false;
+		} else if (!denomination.equals(other.denomination))
+			return false;
+		return true;
 	}
 	
 	// --------------------------------------------------
@@ -32,4 +73,7 @@ public class Carte
 	// --------------------------------------------------
 	// Attribut(s)
 	// --------------------------------------------------
+	
+	CouleurCarte couleur;
+	Denomination denomination;
 }
