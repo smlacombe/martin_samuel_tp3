8d24fc52e1d98d87f6608c180fa3f12b73cdb118
authchir <martin.desharnais@gmail.com>
2011-11-11 14:32:48 -0500

    Implémente le RangPoker.

--- mainDePoker/src/ets/log120/tp3/RangPoker.java
+++ mainDePoker/src/ets/log120/tp3/RangPoker.java
@@ -1,12 +1,32 @@
 package ets.log120.tp3;
 
-public class RangPoker {
+public class RangPoker
+	implements Comparable<RangPoker>{
 
 	// --------------------------------------------------
 	// Constructeur(s)
 	// --------------------------------------------------
 
-	public RangPoker() {
-		
+	public RangPoker(int rang) {
+		this.rang = rang;
 	}
+	
+	// --------------------------------------------------
+	// Accesseur(s)
+	// --------------------------------------------------
+	
+	public int getRang() {
+		return rang;
+	}
+	
+	@Override
+	public int compareTo(RangPoker obj) {
+		return rang - obj.getRang();
+	}
+	
+	// --------------------------------------------------
+	// Attribut(s)
+	// --------------------------------------------------
+	
+	private int rang;
 }
