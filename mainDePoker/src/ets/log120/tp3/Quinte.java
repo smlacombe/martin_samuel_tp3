8e21d64d5f6af97fca5f549a9e0b3b427f4abb7e
authchir <martin.desharnais@gmail.com>
2011-11-12 10:28:45 -0500

    Ajoute la classe Quinte et ses tests.

---
+++ mainDePoker/src/ets/log120/tp3/Quinte.java
@@ -0,0 +1,31 @@
+package ets.log120.tp3;
+
+/**
+ * Classe chargée de reconnaître une quinte.
+ * 
+ * Une quinte est une séquence de cinq cartes, pas toutes de la même couleur. Bien que l'as ait la
+ * dénomination supérieure, il peut être utilisé pour former une séquence as-2-3-4-5. Pourtant, il
+ * doit être toujours au début ou à la fin d'une séquence.
+ * 
+ * @author Martin Desharnais
+ */
+public class Quinte extends AbstractAnalyseurRang {
+	@Override
+	protected boolean reconnaitreMain(ReqAnalyseMain contexte) {
+		Denomination derniereDenomination = null;
+		for(Carte carte : contexte.getMain()) {
+			if (derniereDenomination == null)
+				derniereDenomination = carte.getDenomination();
+			else {
+				int precedant = Denomination.DENOMINATIONS.indexOf(derniereDenomination);
+				int courant   = Denomination.DENOMINATIONS.indexOf(carte.getDenomination());
+				if (precedant != courant - 1
+					&& !(derniereDenomination.equals(Denomination.CINQ) && carte.getDenomination().equals(Denomination.AS)))
+					return false;
+				derniereDenomination = carte.getDenomination();
+			}
+		}
+		contexte.setRangReconnu(new RangPoker(5));
+		return true;
+	}
+}
---
+++ mainDePoker/src/ets/log120/tp3/QuinteTest.java
@@ -0,0 +1,44 @@
+package ets.log120.tp3;
+
+import junit.framework.TestCase;
+
+public class QuinteTest extends TestCase {
+	public void testEvalueMain()
+	{
+		/*
+		 * Test avec main qui n'est pas une quinte
+		 */
+		Main main = new Main();
+		main.add(new Carte(Denomination.TROIS, CouleurCarte.CARREAU));
+		main.add(new Carte(Denomination.NEUF,  CouleurCarte.PIQUE));
+		main.add(new Carte(Denomination.AS,    CouleurCarte.TREFLE));
+		main.add(new Carte(Denomination.DAME,  CouleurCarte.COEUR));
+		main.add(new Carte(Denomination.DEUX,  CouleurCarte.COEUR));
+		
+		assertFalse(new Quinte().reconnaitreMain(new ReqAnalyseMain(main)));
+
+		/*
+		 * Test avec main qui est une quinte avec AS
+		 */
+		main = new Main();
+		main.add(new Carte(Denomination.TROIS,  CouleurCarte.CARREAU));
+		main.add(new Carte(Denomination.CINQ,   CouleurCarte.CARREAU));
+		main.add(new Carte(Denomination.QUATRE, CouleurCarte.CARREAU));
+		main.add(new Carte(Denomination.DEUX,   CouleurCarte.CARREAU));
+		main.add(new Carte(Denomination.AS,     CouleurCarte.CARREAU));
+		
+		assertTrue(new Quinte().reconnaitreMain(new ReqAnalyseMain(main)));
+
+		/*
+		 * Test avec main qui est une quinte sans AS
+		 */
+		main = new Main();
+		main.add(new Carte(Denomination.TROIS,  CouleurCarte.PIQUE));
+		main.add(new Carte(Denomination.CINQ,   CouleurCarte.CARREAU));
+		main.add(new Carte(Denomination.QUATRE, CouleurCarte.TREFLE));
+		main.add(new Carte(Denomination.DEUX,   CouleurCarte.COEUR));
+		main.add(new Carte(Denomination.SIX,    CouleurCarte.CARREAU));
+		
+		assertTrue(new Quinte().reconnaitreMain(new ReqAnalyseMain(main)));
+	}
+}
