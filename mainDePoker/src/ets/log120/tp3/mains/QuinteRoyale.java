b541c7d8716881a62539228f78567d20a15b38f2
authchir <martin.desharnais@gmail.com>
2011-11-12 20:59:21 -0500

    Modifie l'implémentation de QuinteRoyale afin de tirer partie de la classe QuinteCouleur.

--- mainDePoker/src/ets/log120/tp3/mains/QuinteRoyale.java
+++ mainDePoker/src/ets/log120/tp3/mains/QuinteRoyale.java
@@ -15,26 +15,13 @@ import ets.log120.tp3.cartes.Denomination;
 public class QuinteRoyale extends AbstractAnalyseurRang {
 	@Override
 	protected boolean reconnaitreMain(ReqAnalyseMain contexte) {
-		CouleurCarte couleur = null;
-		Denomination derniereDenomination = null;
-		for(Carte carte : contexte.getMain()) {
-			if (couleur == null)
-				couleur = carte.getCouleur();
-			else if (!carte.getCouleur().equals(couleur))
-				return false;
-			
-			if (derniereDenomination == null) {
-				if (carte.getDenomination().equals(Denomination.AS))
-					derniereDenomination = carte.getDenomination();
-				else
-					return false;
-			} else {
-				int precedant = Denomination.DENOMINATIONS.indexOf(derniereDenomination);
-				int courant   = Denomination.DENOMINATIONS.indexOf(carte.getDenomination());
-				if (precedant != courant - 1)
-					return false;
-			}
-		}
+		QuinteCouleur quinteCouleur = new QuinteCouleur();
+		if (!quinteCouleur.reconnaitreMain(contexte))
+			return false;
+		
+		if (!contexte.getMain().first().getDenomination().equals(Denomination.AS))
+			return false;
+		
 		contexte.setRangReconnu(new RangPoker(12));
 		return true;
 	}
