63111cd1436db99cbb228b8a08853ee8d4675dcd
authchir <martin.desharnais@gmail.com>
2011-11-15 14:36:22 -0500

    Ajoute un commentaire à la classe RangPokerBrelan.

--- mainDePoker/src/ets/log120/tp3/mains/RangPokerBrelan.java
+++ mainDePoker/src/ets/log120/tp3/mains/RangPokerBrelan.java
@@ -2,6 +2,15 @@ package ets.log120.tp3.mains;
 
 import ets.log120.tp3.cartes.Denomination;
 
+/**
+ * Classe chargée de différencier deux RangPoker représentant un brelan.
+ * 
+ * En général, c'est la carte « kicker » qui départage les mains. Cette règle s'applique lorsque
+ * deux mains ont la même paire, ou les mêmes deux paires. La carte « kicker » est la carte
+ * supérieure succédant à la paire.
+ * 
+ * @author Martin Desharnais
+ */
 public class RangPokerBrelan extends RangPoker {
 
 	// --------------------------------------------------
