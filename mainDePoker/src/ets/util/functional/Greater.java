b20a038af25cc567b2c6e28193a95293d2e1cbaf
authchir <martin.desharnais@gmail.com>
2011-11-21 23:53:42 -0500

    Ajout de commentaire dans Greater.

--- mainDePoker/src/ets/util/functional/Greater.java
+++ mainDePoker/src/ets/util/functional/Greater.java
@@ -1,7 +1,12 @@
 package ets.util.functional;
 
+/**
+ * Classe permettant d'inverser l'ordre de tri de n'importe quel Comparator.
+ * 
+ * @author Martin Desharnais
+ */
 public class Greater<T extends Comparable<T>>
-	implements java.util.Comparator<T>{
+		implements java.util.Comparator<T> {
 
 	@Override
 	public int compare(T left, T right) {
